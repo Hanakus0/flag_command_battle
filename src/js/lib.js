4a8eb830a5c8c9593894748ffe75df57d1c325c3
nakadakenta <everydoghas12day@outlook.jp>
2025-02-07 16:32:26 +0900

    CHANGE: After github username

--- src/js/lib.js
+++
@@ -1,5 +0,0 @@
-import Message from "./message.js";
-// メッセージ表示用のDOM要素を取得
-// const messageView = document.getElementById("messageView") as HTMLElement;
-Message.printMessage("あれすの攻撃<br>");
-Message.addMessage("トロルに20のダメージを与えた！<br>");
